39182dfde7a03ded80fc1a43930f95d13fb8667e
Jackymancs4 <jackyman_cs4@live.it>
2018-04-15 17:46:11 +0200

    feat: added new commands

--- bin/gitignorer.js
+++ bin/gitignorer.js
@@ -1,24 +1,43 @@
 #!/usr/bin/env node
-const program = require('commander');
 
-const controllers = require('../lib/controllers'); 
+const program = require('commander')
 
-program
-  .version(require('../package.json').version);
+const controllers = require('../lib/controllers')
 
 program
-  .command('init [profile]')
+  .version(require('../package.json').version)
+
+program
+  .command('init [name]')
   .description('Create .gitignore at current directory')
   .option('-f, --force', 'overwrite the existing .gitignore')
-  .action(controllers.init);
+  .option('-g, --global', 'overwrite the existing .gitignore')
+  .option('-p, --profile', 'overwrite the existing .gitignore')
+  .option('-t, --template', 'overwrite the existing .gitignore')
+  .action(controllers.init)
+
+program
+  .command('add [name]')
+  .description('Create .gitignore at current directory')
+  .option('-g, --global', 'overwrite the existing .gitignore')
+  .option('-p, --profile', 'overwrite the existing .gitignore')
+  .option('-t, --template', 'overwrite the existing .gitignore')
+  .action(controllers.add)
 
 program
   .command('profiles')
   .description('List all profiles and their corresponding files')
-  .action(controllers.profiles);
+  .action(controllers.profiles)
 
-program.parse(process.argv);
+program
+  .command('templates')
+  .description('List all profiles and their corresponding files')
+  .option('-s, --src', 'overwrite the existing .gitignore')
+  .option('-p, --pattern', 'overwrite the existing .gitignore')
+  .action(controllers.templates)
+
+program.parse(process.argv)
 
 if (process.argv.length <= 2) {
-  program.help();
+  program.help()
 }
